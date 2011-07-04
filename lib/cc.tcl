c833ac03d65c9144e6b1ee5ea9de40d9154d8544
Steve Bennett <steveb@workware.net.au>
2011-07-05 08:12:32 +1000

    Add make-config-header
    More flexible version of make-autoconf-h
    Bump version to 0.6.2
    
    Signed-off-by: Steve Bennett <steveb@workware.net.au>

--- lib/cc.tcl
+++ lib/cc.tcl
@@ -500,60 +500,90 @@ proc cctest {args} {
 	return $ok
 }
 
-# @make-autoconf-h outfile ?auto-patterns=HAVE_*? ?unquoted-patterns=SIZEOF_*?
+# @make-autoconf-h outfile ?auto-patterns=HAVE_*? ?bare-patterns=SIZEOF_*?
+#
+# Deprecated - see make-config-header
+proc make-autoconf-h {file {autopatterns {HAVE_*}} {barepatterns {SIZEOF_* HAVE_DECL_*}}} {
+	user-notice "*** make-autoconf-h is deprecated -- use make-config-header instead"
+	make-config-header $file -auto $autopatterns -bare $barepatterns
+}
+
+# @make-config-header outfile ?-auto patternlist? ?-bare patternlist? ?-none patternlist? ?-str patternlist? ...
 #
 # Examines all defined variables which match the given patterns
 # and writes an include file, $file, which defines each of these.
-# Variables which match 'auto-patterns' are output as follows:
+# Variables which match '-auto' are output as follows:
 # - defines which have the value "0" are ignored.
 # - defines which have integer values are defined as the integer value.
 # - any other value is defined as a string, e.g. "value"
+# Variables which match '-bare' are defined as-is.
+# Variables which match '-str' are defined as a string, e.g. "value"
+# Variables which match '-none' are omitted.
 #
-# Variables which match 'unquoted-patterns' are defined unquoted.
-# 
+# Note that order is important. The first pattern which matches is selected
+# Default behaviour is:
+#
+#  -bare {SIZEOF_* HAVE_DECL_*} -auto HAVE_* -none *
+#
 # If the file would be unchanged, it is not written.
-proc make-autoconf-h {file {autopatterns {HAVE_*}} {unquotedpatterns {SIZEOF_*}}} {
+proc make-config-header {file args} {
 	set guard _[string toupper [regsub -all {[^a-zA-Z0-9]} [file tail $file] _]]
 	file mkdir [file dirname $file]
 	set lines {}
 	lappend lines "#ifndef $guard"
 	lappend lines "#define $guard"
 
-	# Work out the type of each variable
-	array set types {}
-	foreach pattern $autopatterns {
-		foreach n [array names ::define $pattern] {
-			set types($n) auto
-		}
-	}
-	foreach pattern $unquotedpatterns {
-		foreach n [array names ::define $pattern] {
-			set types($n) unquoted
-		}
-	}
-	foreach n [array names ::define HAVE_DECL_*] {
-		set types($n) decl
-	}
-	foreach n [lsort [array names types]] {
-		if {$types($n) ne "unquoted"} {
-			# Automatically determine the type
-			if {$types($n) eq "auto" && $::define($n) eq "0"} {
-				lappend lines "/* #undef $n */"
+	# Add some defaults
+	lappend args -bare {SIZEOF_* HAVE_DECL_*} -auto HAVE_*
+
+	foreach n [lsort [dict keys [all-defines]]] {
+		set value [get-define $n]
+		set type [calc-define-output-type $n $args]
+		switch -exact -- $type {
+			-bare {
+				# Just output the value unchanged
+			}
+			-none {
 				continue
 			}
-			if {![string is integer -strict $::define($n)]} {
-				lappend lines "#define $n \"$::define($n)\""
+			-str {
+				set value \"$value\"
+			}
+			-auto {
+				# Automatically determine the type
+				if {$value eq "0"} {
+					lappend lines "/* #undef $n */"
+					continue
+				}
+				if {![string is integer -strict $value]} {
+					set value \"$value\"
+				}
+			}
+			"" {
 				continue
 			}
+			default {
+				autosetup-error "Unknown type in make-config-header: $type"
+			}
 		}
-		# Unquoted
-		lappend lines "#define $n $::define($n)"
+		lappend lines "#define $n $value"
 	}
 	lappend lines "#endif"
 	set buf [join $lines \n]
 	write-if-changed $file $buf {
 		msg-result "Created $file"
 	}
+}
+
+proc calc-define-output-type {name spec} {
+	foreach {type patterns} $spec {
+		foreach pattern $patterns {
+			if {[string match $pattern $name]} {
+				return $type
+			}
+		}
+	}
+	return ""
 }
 
 # Initialise some values from the environment or commandline or default settings
