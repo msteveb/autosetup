8e9ec9e96b692efaa61021f196a8dfd7c235e4ed
Steve Bennett <steveb@workware.net.au>
2011-07-11 08:24:27 +1000

    Don't require posix shell
    Solaris only has bourne shell in /bin/sh
    
    Signed-off-by: Steve Bennett <steveb@workware.net.au>

--- lib/init.tcl
+++ lib/init.tcl
@@ -27,8 +27,8 @@ proc autosetup_init {} {
 		} else {
 			writefile configure \
 {#!/bin/sh
-dir="$(dirname "$0")/autosetup"
-WRAPPER="$0" exec $("$dir/find-tclsh" || echo false) "$dir/autosetup" "$@"
+dir="`dirname "$0"`/autosetup"
+WRAPPER="$0" exec `"$dir/find-tclsh" || echo false` "$dir/autosetup" "$@"
 }
 		}
 		catch {exec chmod 755 configure}
