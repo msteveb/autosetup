2d975953c2d91db63332500701ab6788b874f9fa
Steve Bennett <steveb@workware.net.au>
2011-08-05 21:24:28 +1000

    Accept options and settings in any order
    Despite appearances, autoconf configure accepts overrides
    intermixed with options, not just on the right.
    So we do too.
    
    Reported-by: Øyvind Harboe <oyvind.harboe@zylin.com>
    Signed-off-by: Steve Bennett <steveb@workware.net.au>

--- lib/getopt.tcl
+++ lib/getopt.tcl
@@ -5,8 +5,10 @@
 
 # Parse everything out of the argv list which looks like an option
 # Knows about --enable-thing and --disable-thing as alternatives for --thing=0 or --thing=1
+# Everything which doesn't look like an option, or is after --, is left unchanged
 proc getopt {argvname} {
 	upvar $argvname argv
+	set nargv {}
 
 	for {set i 0} {$i < [llength $argv]} {incr i} {
 		set arg [lindex $argv $i]
@@ -16,6 +18,7 @@ proc getopt {argvname} {
 		if {$arg eq "--"} {
 			# End of options
 			incr i
+			lappend nargv {*}[lrange $argv $i end]
 			break
 		}
 
@@ -29,14 +32,14 @@ proc getopt {argvname} {
 			}
 			lappend opts($name) $value
 		} else {
-			break
+			lappend nargv $arg
 		}
 	}
 
-	#puts "getopt: argv=[join $argv] => [join [lrange $argv $i end]]"
+	#puts "getopt: argv=[join $argv] => [join $nargv]"
 	#parray opts
 
-	set argv [lrange $argv $i end]
+	set argv $nargv
 
 	return [array get opts]
 }
