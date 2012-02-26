d0adbdaf3d4e5f71d5d2f4e779f73e7f50f2bd90
Steve Bennett <steveb@workware.net.au>
2012-02-27 09:11:00 +1000

    Better support for solaris
    Signed-off-by: Steve Bennett <steveb@workware.net.au>

--- lib/cc-shared.tcl
+++ lib/cc-shared.tcl
@@ -19,67 +19,79 @@
 
 module-options {}
 
-foreach i {SH_LINKFLAGS SH_CFLAGS SH_LDFLAGS SH_SONAME SHOBJ_CFLAGS SHOBJ_LDFLAGS} {
-	define $i ""
-}
-
-# Some common defaults
-define LD_LIBRARY_PATH LD_LIBRARY_PATH
+# Defaults: gcc on unix
+define SHOBJ_CFLAGS -fpic
+define SHOBJ_LDFLAGS -shared
+define SH_CFLAGS -fpic
+define SH_LDFLAGS -shared
+define SH_LINKFLAGS -rdynamic
 define SH_SOEXT .so
 define SH_SOEXTVER .so.%s
+define SH_SOPREFIX -Wl,-soname,
+define LD_LIBRARY_PATH LD_LIBRARY_PATH
+
+# Note: This is a helpful reference for identifying the toolchain
+#       http://sourceforge.net/apps/mediawiki/predef/index.php?title=Compilers
 
 switch -glob -- [get-define host] {
 	*-*-darwin* {
-		define SH_CFLAGS -dynamic
-		define SH_LDFLAGS "-dynamiclib"
 		define SHOBJ_CFLAGS "-dynamic -fno-common"
 		define SHOBJ_LDFLAGS "-bundle -undefined dynamic_lookup"
-		define SHOBJ_LDFLAGS_R "-bundle"
-		define SH_SOPREFIX "-Wl,-install_name,"
-		define SH_SOEXT ".dylib"
-		define SH_SOEXTVER ".%s.dylib"
+		define SHOBJ_LDFLAGS_R -bundle
+		define SH_CFLAGS -dynamic
+		define SH_LDFLAGS -dynamiclib
+		define SH_LINKFLAGS ""
+		define SH_SOEXT .dylib
+		define SH_SOEXTVER .%s.dylib
+		define SH_SOPREFIX -Wl,-install_name,
 		define LD_LIBRARY_PATH DYLD_LIBRARY_PATH
 	}
 	*-*-ming* - *-*-cygwin - *-*-msys {
-		define LD_LIBRARY_PATH PATH
-		define SH_LDFLAGS -shared
+		define SHOBJ_CFLAGS ""
 		define SHOBJ_LDFLAGS -shared
-		define SHOBJ_LDFLAGS_R -shared
-		define SH_SOEXT ".dll"
+		define SH_CFLAGS ""
+		define SH_LDFLAGS -shared
+		define SH_LINKFLAGS ""
+		define SH_SOEXT .dll
 		define SH_SOEXTVER .dll
+		define SH_SOPREFIX ""
+		define LD_LIBRARY_PATH PATH
 	}
 	sparc* {
-		# gcc on sparc
-		# sparc has a very small GOT table limit, so use -fPIC
-		define SH_LINKFLAGS -rdynamic
-		define SH_CFLAGS -fPIC
-		define SH_LDFLAGS -shared
-		define SHOBJ_CFLAGS -fPIC
-		define SHOBJ_LDFLAGS -shared
+		if {[msg-quiet cc-check-decls __SUNPRO_C]} {
+			msg-result "Found sun stdio compiler"
+			# sun stdio compiler
+			# XXX: These haven't been fully tested. 
+			define SHOBJ_CFLAGS -KPIC
+			define SHOBJ_LDFLAGS "-G"
+			define SH_CFLAGS -KPIC
+			define SH_LINKFLAGS -Wl,-export-dynamic
+			define SH_SOPREFIX -Wl,-h,
+		} else {
+			# sparc has a very small GOT table limit, so use -fPIC
+			define SH_CFLAGS -fPIC
+			define SHOBJ_CFLAGS -fPIC
+		}
 	}
 	*-*-solaris* {
-		# XXX: These haven't been fully tested. 
-		#define SH_LINKFLAGS -Wl,-export-dynamic
-		define SH_CFLAGS -Kpic
-		define SHOBJ_CFLAGS -Kpic
-		define SHOBJ_LDFLAGS "-G"
+		if {[msg-quiet cc-check-decls __SUNPRO_C]} {
+			msg-result "Found sun stdio compiler"
+			# sun stdio compiler
+			# XXX: These haven't been fully tested. 
+			define SHOBJ_CFLAGS -KPIC
+			define SHOBJ_LDFLAGS "-G"
+			define SH_CFLAGS -KPIC
+			define SH_LINKFLAGS -Wl,-export-dynamic
+			define SH_SOPREFIX -Wl,-h,
+		}
 	}
 	*-*-hpux {
 		# XXX: These haven't been tested
-		define SH_LINKFLAGS -Wl,+s
-		define SH_CFLAGS +z
 		define SHOBJ_CFLAGS "+O3 +z"
 		define SHOBJ_LDFLAGS -b
+		define SH_CFLAGS +z
+		define SH_LINKFLAGS -Wl,+s
 		define LD_LIBRARY_PATH SHLIB_PATH
-	}
-	* {
-		# Generic Unix settings
-		define SH_LINKFLAGS -rdynamic
-		define SH_CFLAGS -fpic
-		define SH_LDFLAGS -shared
-		define SH_SOPREFIX "-Wl,-soname,"
-		define SHOBJ_CFLAGS -fpic
-		define SHOBJ_LDFLAGS -shared
 	}
 }
 
