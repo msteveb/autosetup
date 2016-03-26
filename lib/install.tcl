305ee9eb3dfcbe8674127237a3cbfca880bca0ba
Steve Bennett <steveb@workware.net.au>
2016-03-26 14:45:31 +1000

    All .auto files are considered public modules
    Allows support for additional build systems to be added
    
    Signed-off-by: Steve Bennett <steveb@workware.net.au>

--- lib/install.tcl
+++ lib/install.tcl
@@ -10,7 +10,7 @@ proc autosetup_install {dir} {
 
 		set f [open autosetup/autosetup w]
 
-		set publicmodules $::autosetup(libdir)/default.auto
+		set publicmodules [glob $::autosetup(libdir)/*.auto]
 
 		# First the main script, but only up until "CUT HERE"
 		set in [open $::autosetup(dir)/autosetup]
